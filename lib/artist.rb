0da5315a3a1518bbd3eea20352d95b1637a99abd
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-06-27 11:22:10 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+require "pry"
+
+class Artist
+    attr_accessor :name
+
+    # def initialize(name)
+    #     @name ||= name
+    #     # binding.pry        
+    # end
+end
+
+# beyonce = Artist.new("bey")
+# p beyonce.artist
+
+# beyonce.artist = "Yolo"
+# p beyonce.artist
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+require "pry"
+# require_relative 'artist.rb'
+
+class Song
+    attr_accessor :title, :artist
+end
+
+# p artist = Artist.new
+# p artist.name = "Beyonce"
+# p song = Song.new
+# p song.artist = artist
