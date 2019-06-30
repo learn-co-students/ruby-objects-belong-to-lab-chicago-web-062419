953ebf5c2799839a08757dc4fb1a2f41d4081b4b
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-30 09:45:14 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+class Artist
+
+    attr_accessor :name, :song
+
+    def initialize(name = "", song = "")
+        @name = name
+        @song = song
+    end
+
+
+
+
+
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,16 @@
+class Author
+
+    attr_accessor :name, :post
+
+    def initialize(name = "", post = "")
+        @name = name
+        @post = post
+
+    end
+
+
+
+
+
+
+end
